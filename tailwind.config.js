060479a6d3399f7c666f57b04ead65b651901390
suzannaaa <suzanna.23@ichat.sp.edu.sg>
2025-07-18 03:14:02 +0800

    chore: set up Tailwind with Vite

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                silly: ['Kablammo'],
+                hero: ['Sniglet',],
+                chem: ['Creepster'],
+                body: ['"Fira Sans"', 'sans-serif'],
+                mono: ['"Space Mono"', 'monospace'],
+            },
+            colors: {
+                peach: '#ffe0b2',
+                lavender: '#e1bee7',
+                sky: '#b3e5fc',
+            },
+        },
+    },
+    plugins: [],
+}
